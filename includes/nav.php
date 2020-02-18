dedc507cbf6168eda4fa8035c809292effe30339
LOUIS JOSEPH <lj.kyllian@gmail.com>
2020-02-18 09:24:59 +0100

    initialisation_du_projet

---
+++ includes/nav.php
@@ -0,0 +1,13 @@
+<ul>
+	<li><a href="./index.php">Accueil</a></li>
+	<li><a href="./contact.php">Contact</a></li>
+	<li><a href="./contacts.php">Liste des contacts</a></li>
+	<?php
+		if (isset($_SESSION['utilisateur'])) {
+			echo "bienvenue"." ".$_SESSION['utilisateur']['prenom'].' '.$_SESSION['utilisateur']['nom'];
+		}else{
+			echo "<li><a href=\"./seConnecter.php\">se connecter</a></li>";
+		}
+	
+	?>
+</ul>
---
+++ seConnecter.php
@@ -0,0 +1,3 @@
+<?php
+	$titre = 'se connecter';
+?>
