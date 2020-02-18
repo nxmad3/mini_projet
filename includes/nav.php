11492339724e72820457e180ff87670350337b3e
LOUIS JOSEPH <lj.kyllian@gmail.com>
2020-02-18 09:38:08 +0100

    initialisation_du_projet

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,7 @@
 <ul>
 	<li><a href="./index.php">Accueil</a></li>
 	<li><a href="./contact.php">Contact</a></li>
-	<li><a href="./contacts.php">Liste des contacts</a></li>
+	<li><a href="./contacts.php">Liste des contacts mdr</a></li>
 	<?php
 		if (isset($_SESSION['utilisateur'])) {
 			echo "bienvenue"." ".$_SESSION['utilisateur']['prenom'].' '.$_SESSION['utilisateur']['nom'];
